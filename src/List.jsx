0cea8f36b1b2f0181fcb07c844bee435baac2ccc
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-02-25 19:02:45 +0530

    Update List.jsx

--- src/List.jsx
+++ src/List.jsx
@@ -1,1 +1,14 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li> Bc </li>
+      <li> Jam </li>
+      <li> Chowmin </li>
+    </ul>
+  );
+}
+
+export default List;
 
